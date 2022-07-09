645601a111810a2ed09d355c3416eaccc71e1b2f
karenvicent <karenvergara@msn.com>
2022-07-09 03:09:48 +0000

    endpoint signup

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, Usuario
+from api.models import db, Usuario, Rol, Favoritos, FreelancerIdiomas, Idiomas, TipoFreelancer, Experiencia, PerfilFreelancer
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,20 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    correo = request.json.get("correo", None)
+    contrasena = request.json.get("contrasena", None)
+    rol = request.json.get("rol", None)
+
+    if not correo or not contrasena or not rol:
+        return jsonify({'msg': 'Necesitas un correo, una contraseña y un rol para ingresar'}), 404
+
+    usuario_nuevo = Usuario(correo=correo, contrasena=contrasena, is_active=True, rol=int(rol))
+    db.session.add(usuario_nuevo)
+    db.session.commit()
+    respuesta = {
+        "msg" : "usuario registrado"
+    }
+    return jsonify(respuesta), 200
