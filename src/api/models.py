72e7c7e235b7cf49a46ae00667bb8b0ae7fccdd6
karenvicent <karenvergara@msn.com>
2022-07-11 00:30:18 +0000

    endpoints login y rol parte 1

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,10 @@ class Usuario(db.Model):
     contrasena = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     rol = db.Column(db.Integer, db.ForeignKey("t_roles.id"))
+    nombre = db.Column(db.String(150))
+    telefono = db.Column(db.String(50))
+    latitud = db.Column(db.Float)
+    longitud = db.Column(db.Float)
 
     def __repr__(self):
         return f'<User {self.correo}>'
@@ -17,7 +21,12 @@ class Usuario(db.Model):
         return {
             "id": self.id,
             "correo": self.correo,
-            "rol": self.rol
+            "rol": self.rol,
+            "nombre": self.nombre,
+            "telefono": self.telefono,
+            "latitud": self.latitud,
+            "longitud": self.longitud,
+
             # do not serialize the password, its a security breach
         }
 
@@ -26,27 +35,60 @@ class Rol(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(10), nullable=False)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre
+
+            # do not serialize the password, its a security breach
+        }
+
 class Favoritos(db.Model):
     __tablename__ = 't_favoritos'
     id = db.Column(db.Integer, primary_key=True)
     id_empresa = db.Column(db.Integer, db.ForeignKey("t_usuarios.id"))
     id_freelancer = db.Column(db.Integer, db.ForeignKey("t_usuarios.id"))
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_empresa": self.id_empresa,
+            "id_freelancer": self.id_freelancer
+        }
 class FreelancerIdiomas(db.Model):
     __tablename__ = 't_freelancer_idiomas'
     id = db.Column(db.Integer, primary_key=True)
     idioma_id = db.Column(db.Integer, db.ForeignKey("t_idiomas.id"))
     id_freelancer = db.Column(db.Integer, db.ForeignKey("t_usuarios.id"))
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "idioma_id": self.idioma_id,
+            "id_freelancer": self.id_freelancer
+        }
 class Idiomas(db.Model):
     __tablename__ = 't_idiomas'
     id = db.Column(db.Integer, primary_key=True)
     idioma = db.Column(db.String(20))
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "idioma": self.idioma
+        }
+
 class TipoFreelancer(db.Model):
     __tablename__ = 't_tipo_freelancers'
     id = db.Column(db.Integer, primary_key=True)
     tipo = db.Column(db.String(100))
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "tipo": self.tipo
+        }
 
 class Experiencia(db.Model):
     __tablename__ = 't_experiencias'
@@ -54,13 +96,16 @@ class Experiencia(db.Model):
     nombre = db.Column(db.String(20))
     experiencia = db.Column(db.String)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "experiencia": self.experiencia
+        }
+
 class PerfilFreelancer(db.Model):
     __tablename__ = 't_perfil_freelancers'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(150))
-    telefono = db.Column(db.String(50))
-    latitud = db.Column(db.Float)
-    longitud = db.Column(db.Float)
     tipo_freelancer = db.Column(db.Integer, db.ForeignKey("t_tipo_freelancers.id"))
     usuario_id = db.Column(db.Integer, db.ForeignKey("t_usuarios.id"))
     descripcion = db.Column(db.String(10000))
@@ -69,4 +114,16 @@ class PerfilFreelancer(db.Model):
     portafolio = db.Column(db.String(2000))
     tarifa = db.Column(db.Numeric(precision=10, scale=2))
     experiencia_id = db.Column(db.Integer, db.ForeignKey("t_experiencias.id"))
-   
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "tipo_freelancer": self.tipo_freelancer,
+            "usuario_id": self.usuario_id,
+            "descripcion": self.descripcion,
+            "imagen": self.imagen,
+            "linkedin": self.linkedin,
+            "portafolio": self.portafolio,
+            "tarifa": self.tarifa,
+            "experiencia_id": self.experiencia_id,
+        }
