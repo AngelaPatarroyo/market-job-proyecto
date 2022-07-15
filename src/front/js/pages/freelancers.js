bd53e65e995758581e35e21ec9789a9d7768b51b
melbynrojo <angela.patarroyo@hotmail.com>
2022-07-15 19:53:21 +0000

    freelancers perfil

---
+++ src/front/js/pages/freelancers.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Profile from "../../img/Profile.png";
+import "../../styles/home.css";
+
+export const Freelancers = () => {
+  return (
+    <div className="container fluid">
+      <div id="freelancerscon" className="justify-content-center d-flex">
+        <div className="w-50 ml-5 d-flex justify-content-center">
+          <img src={Profile} style={{ height: "350px", width: "350px" }} />
+        </div>
+        <div
+          className="w-50 text-left"
+          style={{ height: "350px", width: "350px" }}
+        >
+          <h1>Ivan González</h1>
+          <h4>Diseñador Gráfico</h4>
+          <h5>
+            Calificación <i className="fab fa-github"></i> 4.5
+          </h5>
+          <h5>3 - 5 años de experiencia</h5>
+          <h5>Idiomas: Español, Inglés</h5>
+          <h5>Tarifa: $35</h5>
+          <div className="container d-grid gap-2 d-md w-50 row align-items-start p-0 mt-5">
+            <button type="button" className="btn btn btn-dark">
+              GitHub
+            </button>
+            <button type="button" className="btn btn btn-dark">
+              LinkedIn
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="container mt-5 col-4"
+        style={{ width: "1000px", height: "1000px" }}
+      >
+        <h3>Descripción</h3>
+        <p className="mt-5 mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </p>
+        <div className="d-flex justify-content-center">
+          <button type="button" className="btn btn-dark btn-lg col-4">
+            Contratar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
