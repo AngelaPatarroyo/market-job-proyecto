b3edfcd7776b302e2c3f28802d8710b7c05c5041
karenvicent <karenvergara@msn.com>
2022-07-28 01:53:40 +0000

    token

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
+from datetime import timedelta
 
 #from models import Person
 
@@ -21,8 +22,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt = JWTManager(app)
-
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
